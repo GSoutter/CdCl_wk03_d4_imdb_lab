437b38848856a17a3d1aab995d022c9fa35c1eb3
Gordon Soutter <gordon.soutter@outlook.com>
2020-03-26 15:10:40 +0000

    intial commit. Files and sql statement added

---
+++ models/star.rb
@@ -0,0 +1,17 @@
+
+
+
+class Star
+
+  attr_accessor :first_name, :last_name
+  attr_reader :id
+
+
+  def initialize(options)
+    @first_name = options['first_name']
+    @last_name = options['last_name']
+    @id = options['id'].to_i if options['id']
+  end
+
+
+end
